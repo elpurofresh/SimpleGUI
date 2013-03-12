f67eeca744ec1e02f6378597b5a95e27de42828c
Andres Mora <amorav@gmail.com>
2013-03-12 16:01:13 -0700

    Simple GUI for tests

---
+++ guiMain/guiMain/GuiMain.java
@@ -0,0 +1,326 @@
+package guiMain;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+
+public class GuiMain {
+
+	SerialPortManager serialPortManager = null;
+	private JFrame frame;
+	
+	JLabel lblMainTitle = new JLabel("Underwater Communications");
+	JLabel lblDataOut = new JLabel("Data Out");
+	JLabel lblDataIn = new JLabel("Data In");
+	JLabel lblSelectComm = new JLabel("Select Comm ->");
+	JComboBox cboxPorts = new JComboBox();
+	JButton btnConnect = new JButton("Connect");
+	JButton btnDisconnect = new JButton("Disconnect");
+	JLabel lblStringOut = new JLabel("String out");
+	JTextField textInterval = new JTextField();
+	JTextField textOutputTest = new JTextField();
+	JLabel lblArrow = new JLabel("=>");
+	JScrollPane scrollPaneOutput = new JScrollPane();
+	JTextArea textOutputArea = new JTextArea();
+	JLabel lblInterval = new JLabel("Interval (sec):");
+	JButton btnStartComm = new JButton("START COMM");
+	JButton btnStopComm = new JButton("STOP COMM");
+	JLabel lblBer = new JLabel("BER: ");
+	JLabel lblBerValue = new JLabel("0");
+	JTextArea textInputArea = new JTextArea();
+	JScrollPane scrollPaneInput = new JScrollPane();
+	JLabel lblControlPanel = new JLabel("Control Panel");
+	JTextArea textMsgArea = new JTextArea();
+	JScrollPane scrollPaneMsg = new JScrollPane();
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiMain window = new GuiMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiMain() {
+		initialize();
+		serialPortManager = new SerialPortManager(this);
+		serialPortManager.searchForPorts();
+	}
+	
+	public void toggleControls(){
+		if (serialPortManager.getConnectionStatus()) {
+			
+			cboxPorts.setEnabled(false);
+			btnConnect.setEnabled(false);
+			
+			btnDisconnect.setEnabled(true);
+			btnStartComm.setEnabled(true);
+			btnStopComm.setEnabled(true);
+			textOutputTest.setEnabled(true);
+			textInterval.setEnabled(true);
+		}
+		else {
+			cboxPorts.setEnabled(true);
+			btnConnect.setEnabled(true);
+			
+			btnDisconnect.setEnabled(false);
+			btnStartComm.setEnabled(false);
+			btnStopComm.setEnabled(false);
+			textOutputTest.setEnabled(false);
+			textInterval.setEnabled(false);
+		}
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1037, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{178, 194, 0, 122, 169, 152, 93, 26};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 26, 0, 23, 31, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		
+		GridBagConstraints gbc_lblUnderwaterCommunications = new GridBagConstraints();
+		gbc_lblUnderwaterCommunications.gridwidth = 8;
+		gbc_lblUnderwaterCommunications.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUnderwaterCommunications.gridx = 0;
+		gbc_lblUnderwaterCommunications.gridy = 0;
+		frame.getContentPane().add(lblMainTitle, gbc_lblUnderwaterCommunications);
+		
+		GridBagConstraints gbc_lblControlPanel = new GridBagConstraints();
+		gbc_lblControlPanel.gridwidth = 2;
+		gbc_lblControlPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblControlPanel.gridx = 0;
+		gbc_lblControlPanel.gridy = 1;
+		frame.getContentPane().add(lblControlPanel, gbc_lblControlPanel);
+		
+		
+		GridBagConstraints gbc_lblDataOut = new GridBagConstraints();
+		gbc_lblDataOut.gridwidth = 2;
+		gbc_lblDataOut.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataOut.gridx = 3;
+		gbc_lblDataOut.gridy = 1;
+		frame.getContentPane().add(lblDataOut, gbc_lblDataOut);
+		frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblDataOut, lblDataIn, textOutputTest}));
+		
+		
+		GridBagConstraints gbc_lblDataIn = new GridBagConstraints();
+		gbc_lblDataIn.gridwidth = 2;
+		gbc_lblDataIn.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataIn.gridx = 5;
+		gbc_lblDataIn.gridy = 1;
+		frame.getContentPane().add(lblDataIn, gbc_lblDataIn);
+		
+		
+		GridBagConstraints gbc_lblSelectComm = new GridBagConstraints();
+		gbc_lblSelectComm.anchor = GridBagConstraints.EAST;
+		gbc_lblSelectComm.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSelectComm.gridx = 0;
+		gbc_lblSelectComm.gridy = 2;
+		frame.getContentPane().add(lblSelectComm, gbc_lblSelectComm);
+		
+		
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 2;
+		frame.getContentPane().add(cboxPorts, gbc_comboBox);
+		
+		
+		GridBagConstraints gbc_scrollPaneInput = new GridBagConstraints();
+		gbc_scrollPaneInput.gridwidth = 3;
+		gbc_scrollPaneInput.fill = GridBagConstraints.BOTH;
+		gbc_scrollPaneInput.gridheight = 6;
+		gbc_scrollPaneInput.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPaneInput.gridx = 5;
+		gbc_scrollPaneInput.gridy = 2;
+		frame.getContentPane().add(scrollPaneInput, gbc_scrollPaneInput);
+		textInputArea.setRows(9);
+		textInputArea.setColumns(10);
+		scrollPaneInput.setViewportView(textInputArea);
+		
+		GridBagConstraints gbc_scrollPaneMsg = new GridBagConstraints();
+		gbc_scrollPaneMsg.gridheight = 2;
+		gbc_scrollPaneMsg.fill = GridBagConstraints.BOTH;
+		gbc_scrollPaneMsg.gridwidth = 2;
+		gbc_scrollPaneMsg.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPaneMsg.gridx = 0;
+		gbc_scrollPaneMsg.gridy = 3;
+		frame.getContentPane().add(scrollPaneMsg, gbc_scrollPaneMsg);
+		textMsgArea.setRows(2);
+		textMsgArea.setColumns(10);
+		scrollPaneMsg.setViewportView(textMsgArea);
+		
+		GridBagConstraints gbc_btnConnect = new GridBagConstraints();
+		gbc_btnConnect.insets = new Insets(0, 0, 5, 5);
+		gbc_btnConnect.gridx = 0;
+		gbc_btnConnect.gridy = 5;
+		frame.getContentPane().add(btnConnect, gbc_btnConnect);
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				serialPortManager.connect();
+				if (serialPortManager.getConnectionStatus() == true)
+				{
+					if (serialPortManager.initIOStream() == true)
+					{
+						serialPortManager.initListener();
+					}
+				}
+				textOutputTest.setText("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+			}
+		});
+		
+		GridBagConstraints gbc_btnDisconnect = new GridBagConstraints();
+		gbc_btnDisconnect.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDisconnect.gridx = 1;
+		gbc_btnDisconnect.gridy = 5;
+		frame.getContentPane().add(btnDisconnect, gbc_btnDisconnect);
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				serialPortManager.disconnect();
+				textInputArea.setText("");
+				textOutputArea.setText("");
+			}
+		});
+		
+		
+		GridBagConstraints gbc_lblStringOut = new GridBagConstraints();
+		gbc_lblStringOut.insets = new Insets(0, 0, 5, 5);
+		gbc_lblStringOut.gridx = 0;
+		gbc_lblStringOut.gridy = 6;
+		frame.getContentPane().add(lblStringOut, gbc_lblStringOut);
+		
+		
+		textOutputTest.setText("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		GridBagConstraints gbc_outputText = new GridBagConstraints();
+		gbc_outputText.fill = GridBagConstraints.HORIZONTAL;
+		gbc_outputText.insets = new Insets(0, 0, 5, 5);
+		gbc_outputText.gridx = 1;
+		gbc_outputText.gridy = 6;
+		frame.getContentPane().add(textOutputTest, gbc_outputText);
+		textOutputTest.setColumns(10);
+		textOutputTest.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent evt) {
+			//	if (evt.getSource() == textOutputTest) {
+					textOutputTest.setText("");
+			//	}
+			}
+		});
+		
+		
+		GridBagConstraints gbc_lblArrow = new GridBagConstraints();
+		gbc_lblArrow.insets = new Insets(0, 0, 5, 5);
+		gbc_lblArrow.gridx = 2;
+		gbc_lblArrow.gridy = 6;
+		frame.getContentPane().add(lblArrow, gbc_lblArrow);
+		
+		
+		GridBagConstraints gbc_scrollPaneOutput = new GridBagConstraints();
+		gbc_scrollPaneOutput.gridwidth = 2;
+		gbc_scrollPaneOutput.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPaneOutput.fill = GridBagConstraints.BOTH;
+		gbc_scrollPaneOutput.gridheight = 6;
+		gbc_scrollPaneOutput.gridx = 3;
+		gbc_scrollPaneOutput.gridy = 2;
+		frame.getContentPane().add(scrollPaneOutput, gbc_scrollPaneOutput);
+		scrollPaneOutput.setViewportView(textOutputArea);
+		textOutputArea.setRows(9);
+		textOutputArea.setColumns(10);
+		
+		
+		GridBagConstraints gbc_lblInterval = new GridBagConstraints();
+		gbc_lblInterval.insets = new Insets(0, 0, 5, 5);
+		gbc_lblInterval.gridx = 0;
+		gbc_lblInterval.gridy = 7;
+		frame.getContentPane().add(lblInterval, gbc_lblInterval);
+		
+		
+		textInterval.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+			}
+		});
+		textInterval.setText("0");
+		GridBagConstraints gbc_textInterval = new GridBagConstraints();
+		gbc_textInterval.insets = new Insets(0, 0, 5, 5);
+		gbc_textInterval.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textInterval.gridx = 1;
+		gbc_textInterval.gridy = 7;
+		frame.getContentPane().add(textInterval, gbc_textInterval);
+		textInterval.setColumns(10);
+		
+		
+		btnStartComm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+					serialPortManager.sendData(textOutputTest.getText());
+			}
+		});
+		GridBagConstraints gbc_btnStartComm = new GridBagConstraints();
+		gbc_btnStartComm.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnStartComm.insets = new Insets(0, 0, 0, 5);
+		gbc_btnStartComm.gridx = 0;
+		gbc_btnStartComm.gridy = 8;
+		frame.getContentPane().add(btnStartComm, gbc_btnStartComm);
+		
+		btnStopComm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnStopComm = new GridBagConstraints();
+		gbc_btnStopComm.fill = GridBagConstraints.BOTH;
+		gbc_btnStopComm.insets = new Insets(0, 0, 0, 5);
+		gbc_btnStopComm.gridx = 1;
+		gbc_btnStopComm.gridy = 8;
+		frame.getContentPane().add(btnStopComm, gbc_btnStopComm);
+		
+		GridBagConstraints gbc_lblBertotalWrongtotal = new GridBagConstraints();
+		gbc_lblBertotalWrongtotal.insets = new Insets(0, 0, 0, 5);
+		gbc_lblBertotalWrongtotal.anchor = GridBagConstraints.WEST;
+		gbc_lblBertotalWrongtotal.gridx = 3;
+		gbc_lblBertotalWrongtotal.gridy = 8;
+		frame.getContentPane().add(lblBer, gbc_lblBertotalWrongtotal);
+		
+		GridBagConstraints gbc_label_1BerValue = new GridBagConstraints();
+		gbc_label_1BerValue.insets = new Insets(0, 0, 0, 5);
+		gbc_label_1BerValue.gridx = 4;
+		gbc_label_1BerValue.gridy = 8;
+		frame.getContentPane().add(lblBerValue, gbc_label_1BerValue);
+	}
+
+}
---
+++ guiMain/guiMain/SerialPortManager.java
@@ -0,0 +1,329 @@
+package guiMain;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.TooManyListenersException;
+
+public class SerialPortManager implements SerialPortEventListener{
+
+	//passed from main GUI
+	GuiMain window = null;
+
+	//for containing the ports that will be found
+	@SuppressWarnings("rawtypes")
+	private Enumeration ports = null;
+	//map the port names to CommPortIdentifiers
+	private HashMap<String, CommPortIdentifier> portMap = new HashMap<String, CommPortIdentifier>();
+
+	//this is the object that contains the opened port
+	private CommPortIdentifier selectedPortIdentifier = null;
+	private SerialPort serialPort = null;
+
+	//input and output streams for sending and receiving data
+	private InputStream input = null;
+	private OutputStream output = null;
+
+	//just a boolean flag that i use for enabling
+	//and disabling buttons depending on whether the program
+	//is connected to a serial port or not
+	private boolean bConnected = false;
+
+	//the timeout value for connecting with the port
+	final static int TIMEOUT = 2000;
+
+	//some ascii values for for certain things
+	final static int SPACE_ASCII = 32;
+	final static int DASH_ASCII = 45;
+	final static int NEW_LINE_ASCII = 10;
+	final static int START_BYTE = 91; // '['
+	final static int STOP_BYTE = 93; // ']'
+	final static int ASCII_VALUE_OF_ZERO = 48;
+
+	//a string for recording what goes on in the program
+	//this string is written to the GUI
+	String logText = "";
+
+	boolean dataPacketFlag = false;
+	private int byteCounter = 0;
+	byte netText[] = new byte[200];
+
+	public int numRxD = 0;
+	public int numTxD = 0;
+
+	public SerialPortManager(GuiMain window){
+		this.window = window;
+	}
+
+	public void searchForPorts(){
+		ports = CommPortIdentifier.getPortIdentifiers();
+
+		while (ports.hasMoreElements()) {
+			CommPortIdentifier currentPort = (CommPortIdentifier) ports.nextElement();
+			if (currentPort.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				window.cboxPorts.addItem(currentPort.getName());
+				portMap.put(currentPort.getName(), currentPort);
+			}
+		}
+	}
+
+	public void connect(){
+		String selectedPort = (String)window.cboxPorts.getSelectedItem();
+		selectedPortIdentifier = (CommPortIdentifier)portMap.get(selectedPort);
+
+		CommPort commPort = null;
+
+		try {
+			commPort = selectedPortIdentifier.open("UWSN_ControlPanel", TIMEOUT);
+			serialPort = (SerialPort)commPort;
+			serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+			setConnected(true);
+
+			logText = selectedPort + " opened successfully!";
+			window.textMsgArea.setForeground(Color.black);
+			window.textMsgArea.append(logText + "\n");
+
+			window.toggleControls();
+
+		} catch (PortInUseException e) {
+			logText = selectedPort + " is in use. (" +e.toString() + ")";
+
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+		} catch (Exception e) {
+			logText = "Failed to open " + selectedPort + "(" + e.toString() + ")";
+
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+		}
+	}
+
+	public boolean initIOStream(){
+
+		boolean successful = false;
+
+		try {
+			input 	= serialPort.getInputStream();
+			output 	= serialPort.getOutputStream();
+
+			successful = true;
+			return successful;
+
+		} catch (IOException e) {
+			logText = "I/O Streams failed to open. (" + e.toString() + ")";
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+			return successful;
+		}
+	}
+
+	public void initListener(){
+
+		try {
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+
+		} catch (TooManyListenersException e) {
+			logText = "Too many listeners. (" + e.toString() + ")";
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+		}		
+	}
+
+	public void disconnect(){
+		try {
+			serialPort.removeEventListener();
+			serialPort.close();
+			input.close();
+			output.close();
+
+			setConnected(false);
+			window.toggleControls();
+
+			logText = "Disconnected.";
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+
+
+		} catch (Exception e) {
+			logText = "Failed to close " + serialPort.getName() + "(" + e.toString() + ")";
+			window.textMsgArea.setForeground(Color.red);
+			window.textMsgArea.append(logText + "\n");
+			//e.printStackTrace();
+		}
+	}
+
+	final public boolean getConnectionStatus()
+	{
+		return bConnected;
+	}
+
+	public void setConnected(boolean bConnected)
+	{
+		this.bConnected = bConnected;
+	}
+
+	public void serialEvent(SerialPortEvent spe) {
+		char[] end = new char[1];
+		end[0] = ']';
+
+		if (spe.getEventType() == SerialPortEvent.DATA_AVAILABLE) {//If one byte of data came in
+
+			try {
+				byte charVal = (byte)input.read();
+
+				netText[byteCounter++] = charVal;
+				window.textInputArea.append(new String(new byte[] {(byte) charVal}));
+
+				/*if (byteCounter > 38) {
+					byteCounter = 0;					
+					window.textInputArea.append("\n");
+				}*/
+				/*if (netText[byteCounter] == end[0]) {
+					for (int i = 0; i < netText.length; i++) {
+						String charToTest = new String(new byte[] {(byte) netText[i]});
+						if (window.textMsgArea.getText().compareTo(charToTest.charAt(0)) {
+							
+						}
+					}
+				}*/
+
+			} catch (Exception e) {
+				logText = "Failed to read data." + "(" + e.toString() + ")";
+				window.textInputArea.setForeground(Color.red);
+				window.textInputArea.append(logText + "\n");
+				System.err.println(e.toString());
+			}
+		}
+	}
+
+	/*public void serialEvent(SerialPortEvent spe) {
+
+		if (spe.getEventType() == SerialPortEvent.DATA_AVAILABLE) {//If one byte of data came in
+
+
+			try {
+				byte charVal = (byte)input.read();
+
+				if (charVal == START_BYTE && dataPacketFlag == false) { //Stores start byte
+					dataPacketFlag = true;
+
+					netText[byteCounter++] = charVal;
+
+					window.textInputArea.append("RxD-");
+					logText = new String(new byte[] {charVal});
+					window.textInputArea.append(logText);
+				}
+
+				else if (charVal != STOP_BYTE && dataPacketFlag == true) { //Stores actual data
+					logText = new String(new byte[] {charVal});
+					window.textInputArea.append(logText);
+
+					netText[byteCounter++] = charVal;					
+				}
+
+				else if (charVal == STOP_BYTE && dataPacketFlag == true) { //Stores stop byte
+					dataPacketFlag = false;
+
+					//if (netText[1] != msgToChildren[0]) { //botID parent...						
+					printChildMsg(byteCounter);
+					numRxD++;
+					//} 
+
+					logText = new String(new byte[] {charVal});
+					window.textInputArea.append(logText);
+					window.textInputArea.append("\n");
+
+					byteCounter = 0;
+				}
+
+			} catch (Exception e) {
+				logText = "Failed to read data." + "(" + e.toString() + ")";
+				window.textInputArea.setForeground(Color.red);
+				window.textInputArea.append(logText + "\n");
+				System.err.println(e.toString());
+			}
+		}
+	}*/
+
+	public static byte[] stringToBytesASCII(String str) {
+		byte[] b = new byte[str.length()];
+		for (int i = 0; i < b.length; i++) {
+			b[i] = (byte) str.charAt(i);
+		}
+		return b;
+	}
+
+	public void sendData(String dataToSend){
+
+		//int[] msgToChildren = new int[20]; 
+		//String test = "012903405sdfgsdgsdjkg";
+		//byte[] dataBytes = new byte[dataToSend.length()];
+		//dataBytes = dataToSend.getBytes();
+
+		byte[] dataBytesOut = stringToBytesASCII(dataToSend);
+
+		try {
+			output.write(START_BYTE);
+			System.out.print("Data Sent: [");
+
+			window.textOutputArea.append("TxD-");
+			logText = new String(new byte[] {(byte) START_BYTE});
+			window.textOutputArea.append(logText);
+
+			//output.write(dataToSend.getBytes());
+			//window.textOutputArea.append(new String(dataToSend.getBytes()));
+			//System.out.print(dataToSend);
+			for (int i = 0; i < dataBytesOut.length; i++) {
+				output.write(dataBytesOut[i]);
+				window.textOutputArea.append(new String(new byte[] {(byte) dataBytesOut[i]}));
+				//System.out.print(new String(new byte[] {(byte) dataOut[i]}));
+				System.out.print(dataBytesOut[i]);
+				//window.textOutputArea.append(new String(new byte[] {(byte) msgToChildren[i]}));
+				//System.out.print(new String(new byte[] {(byte) msgToChildren[i]}));
+			}
+
+			output.write(STOP_BYTE); 
+			output.flush();
+			numTxD++;
+
+			logText = new String(new byte[] {(byte) STOP_BYTE});
+			window.textOutputArea.append(logText + "\n");
+			System.out.print(new String(new byte[] {STOP_BYTE}) + "\n");
+
+		} catch (Exception e) {
+			logText = "Failed to write data. (" + e.toString() + ")";
+			window.textOutputArea.setForeground(Color.red);
+			window.textOutputArea.append(logText + "\n");
+		}
+	}
+
+	public void printChildMsg(int byteCounter){
+
+		for (int i = 0; i < byteCounter; i++) {
+			window.textInputArea.append(new String(new byte[] {netText[i]}) + " ");
+		}
+		window.textInputArea.append("\n");
+	}
+
+	public int getNumRxD() {
+		return numRxD;
+	}
+
+	public int getNumTxD() {
+		return numTxD;
+	}
+
+
+}
